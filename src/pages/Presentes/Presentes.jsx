d75b82902b74803c32d7f9f1373cae007e378cab
agripe049 <agripeheber@gmail.com>
2025-07-16 23:58:24 -0300

    Ajustando tela de presentes

--- src/pages/Presentes/Presentes.jsx
+++ src/pages/Presentes/Presentes.jsx
@@ -1,11 +1,40 @@
-import "./Presentes.css"
+import { useState } from "react";
+import "./Presentes.css";
+import qrcode from "../../assets/qrcode.jpeg";
 
-function Presentes () {
+function Presentes() {
+  const chavePix = "goncalvesmiguelluis@gmail.com";
+  const [copiado, setCopiado] = useState(false);
+
+  const copiarPix = () => {
+    navigator.clipboard.writeText(chavePix);
+    setCopiado(true);
+    setTimeout(() => setCopiado(false), 2000);
+  };
+
+
   return (
     <div className="interface-presentes">
-        <h1>Presentes</h1>
+
+      <h1>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          Clique aqui para ver nossa lista de presentes!
+        </a>
+      </h1>
+
+      <p>Ou, se preferir:</p>
+      <h2>Nos presenteie com um Pix</h2>
+      <img src={qrcode} alt="QR Code para presente de casamento" className="qr-code"/>
+
+      <p>Chave Pix (e-mail):</p>
+      <br />
+      <pre className="codigo-pix">{chavePix}</pre>
+      <br />
+      <button onClick={copiarPix} className="botao-copiar">
+          {copiado ? "Copiado!" : "Copiar chave pix"}
+      </button>
     </div>
-  )
+  );
 }
 
 export default Presentes;
